25ca411f3bf9616cda9028027a6fac8d6c25ec87
Manasses <manasses.oliveira@dsbsolucoes.com.br>
2023-03-20 19:51:19 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aws_codedeploy_windows_scripts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
